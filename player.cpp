150453cea26294cbe174dafd6d888810dbae431b
bocekeye <yokoshou3236@icloud.com>
2022-09-28 11:14:15 +0900

    ジャンプの実装の修正と当たり判定の修正

--- player.cpp
+++ player.cpp
@@ -2,13 +2,25 @@
 #include "game.h"
 #include "player.h"
 
+namespace
+{	
+	//キャラクターのサイズ
+	constexpr float kSizeX = 128.0f;
+	constexpr float kSizeY = 128.0f;
+
+	//ジャンプ力
+	constexpr float kJumpAcc = -16.0f;
+	//重力
+	constexpr float kGravity = 0.5f;
+}
+
 Player::Player()
 {
 	m_handle = -1;
 	m_fieldY = 0.0f;
 
-	m_isJumpUp = false;
-	m_isJumpDown = false;
+//	m_isJumpUp = false;
+//	m_isJumpDown = false;
 
 	m_isDead = false;
 }
@@ -30,34 +42,56 @@ void Player:: setup(float fieldY)
 	m_vec.y = 0.0f;
 }
 
-void Player::update()
+void Player::update()//1秒間に60回呼ばれている
 {
-//	m_pos += m_vec;
+	
+	m_pos += m_vec;
 
+	//地面とのプレイヤー移動制限
+	bool isField = false; //地面と当たっていない
+
+	if (m_pos.y > m_fieldY - m_graphSize.y)
+	{
+		m_pos.y = m_fieldY - m_graphSize.y;
+		isField = true;
+	}
+
+	if (m_isDead) return;
 	// キー入力処理
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (padState & PAD_INPUT_1)
 	{
-		m_isJumpUp = true;
+		if (isField) //地面に当たったらジャンプができる
+		{
+			m_vec.y = kJumpAcc; //ジャンプ開始
+		}
+		
 	}
+	m_vec.y += kGravity;
 
-	if (m_isJumpUp)
-	{
-		m_pos.y -= 4.0f;
-		if (m_pos.y <= 64.0f)
-		{
-			m_isJumpUp = false;
-			m_isJumpDown = true;
-		}
-	}
-	else if (m_isJumpDown)
-	{
-		m_pos.y += 4.0f;
-		if (m_pos.y >= m_fieldY - m_graphSize.y)
-		{
-			m_pos.y = m_fieldY - m_graphSize.y;
-		}
-	}
+
+
+	//if (padState & PAD_INPUT_1)
+	//{
+	//	m_isJumpUp = true;
+	//}
+	//if (m_isJumpUp)
+	//{
+	//	m_pos.y -= 4.0f;
+	//	if (m_pos.y <= 64.0f)
+	//	{
+	//		m_isJumpUp = false;
+	//		m_isJumpDown = true;
+	//	}
+	//}
+	//else if (m_isJumpDown)
+	//{
+	//	m_pos.y += 4.0f;
+	//	if (m_pos.y >= m_fieldY - m_graphSize.y)
+	//	{
+	//		m_pos.y = m_fieldY - m_graphSize.y;
+	//	}
+	//}
 
 }
 
@@ -76,9 +110,9 @@ void Player::draw()
 bool Player::isCol(Car& car)
 {
 	float playerLeft = getPos().x;
-	float playerRight = getPos().x + getColSize().x;
+	float playerRight = getPos().x + kSizeX;
 	float playerTop = getPos().y;
-	float playerBottom = getPos().y + getColSize().y;
+	float playerBottom = getPos().y + kSizeY;
 
 	float carLeft = car.getPos().x;
 	float carRight = car.getPos().x + car.getSize().x;
